20d5366f48bf1525e00a8c58e9c1615d146de7b0
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-01-05 16:37:15 +1030

    Questions answers added

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+/**
+ * Declaration of Question Arrays with options, and answer.
+ */
+var questions = [
+    {
+        key: 1,
+        question: "Q1. What is the fullform of HTML?",
+        answer: "Hyper Text Markup Language",
+        options: ["Hypersonic Type Markup Language", "Hyper Text Markup Language",
+            "High Text Modern Language", "None of these"]
+    },
+    {
+        key: 2,
+        question: "Q2. What is Fullform of CSS?",
+        answer: "Cascading Style Sheet",
+        options: ["None of these", "Code Style Sheet", "Cascade Short Sheet ", "Cascading Style Sheet"]
+
+    },
+    {
+        key: 3,
+        question: "Q3. What is the Fullform of GMAIL?",
+        answer: "Google Mail",
+        options: ["None of these", "Gadget Mailing", "Google Mail", "Go Mail"]
+
+    },
+    {
+        key: 4,
+        question: "Q4. What is the Fullform of OS?",
+        answer: "Operating System",
+        options: ["Operation System", "Operating System", "Opcode System", "None of these"]
+    },
+    {
+        key: 5,
+        question: "Q5. What is the fullform of JS",
+        answer: "Javascript",
+        options: ["Javascript", "None of these", "Jazzscript", "Joinscript"]
+    }
+];
